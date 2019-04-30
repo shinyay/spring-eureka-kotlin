af01c4d47a4615e9b75ae1c626d640e4503fb292
shinyay <shinya.com@gmail.com>
2019-04-30 12:06:37 +0900

    Modify implementation for greeting interface

--- src/main/kotlin/io/pivotal/shinyay/eureka/EurekaClientApplication.kt
+++ src/main/kotlin/io/pivotal/shinyay/eureka/EurekaClientApplication.kt
@@ -1,10 +1,22 @@
 package io.pivotal.shinyay.eureka
 
+import com.netflix.discovery.EurekaClient
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-class EurekaClientApplication
+@RestController
+class EurekaClientApplication : GreetingInterface {
+
+	private lateinit var eurekaClient: EurekaClient
+
+	@Value("\${spring.application.name}")
+	private lateinit var appName: String
+
+	override fun greeting(): String = String.format("Hello from '%s'!", eurekaClient.getApplication(appName).name)
+}
 
 fun main(args: Array<String>) {
 	runApplication<EurekaClientApplication>(*args)
